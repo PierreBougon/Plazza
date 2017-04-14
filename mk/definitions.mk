2f48daad5e227c6ca735a301f6fb5a90152ae3e4
PierreBougon <pierre.bougon@epitech.eu>
2017-04-15 00:57:18 +0200

    mk directory done

---
+++ mk/definitions.mk
@@ -0,0 +1,49 @@
+
+## Some basic C++ definitions for arcade Makefiles
+
+# MODE
+
+DEBUG	=	NO
+
+RELEASE	=	NO
+
+DLL	=	NO
+
+SANITIZE =	NO
+
+# NAME
+
+PROJECT_NAME	=
+
+# Compilation definitions
+
+CXXFLAGS	=	-std=c++14 -W -Wall -Wextra
+ifeq ($(RELEASE), YES)
+CXXFLAGS	+=	-Werror
+else
+ifeq ($(DEBUG), YES)
+CXXFLAGS	+=	-D DEBUG -g
+endif
+endif
+ifeq ($(DLL), YES)
+CXXFLAGS	+=	-fPIC -ldl
+endif
+ifeq ($(STATIC), YES)
+CXXFLAGS	+=	-fPIC
+endif
+
+ifeq ($(SANITIZE), YES)
+CXXFLAGS	+=	-fsanitize=memory -fsanitize-memory-track-origins=2 -fomit-frame-pointer
+endif
+
+CXX		=	g++
+
+LDFLAGS	=
+
+ifeq ($(DLL), YES)
+CXX	+= -shared
+endif
+
+ifeq ($(STATIC), YES)
+LINKER = ar -rcs
+endif
---
+++ mk/rules.mk
@@ -0,0 +1,37 @@
+## Generic rules for Makefiles
+
+$(NAME):	$(OBJ)
+		@$(ECHO)
+		@$(ECHO) "$(BLUE) Compiled with : $(BOLD_WHITE)$(CXX)$(CLEAR)"
+		@$(ECHO) "$(BLUE) Flags used \t  : $(BOLD_WHITE)$(CXXFLAGS)$(CLEAR)"
+		@$(ECHO) "$(BLUE) Dependencies  :$(BOLD_WHITE)$(LDFLAGS)$(CLEAR)"
+		@$(ECHO)
+		@$(ECHO) "$(BLUE) ==== $(BOLD_WHITE) $(PROJECT_NAME)  Compiled $(BLUE) ==== $(CLEAR)\n\n"
+ifeq ($(STATIC), YES)
+		@$(LINKER) -o $(NAME) $(OBJ)
+else
+		@$(CXX) $(OBJ) -o $(NAME) $(INC) $(LDFLAGS)
+endif
+		@$(eval PROJECT_NAME=)
+		@$(eval LDFLAGS=)
+		@$(eval DLL=NO)
+		@$(eval CXXFLAGS= -std=c++14 -W -Wall -Wextra)
+
+all:		$(NAME)
+
+clean:
+		@$(ECHO)
+		@$(ECHO) "$(BOLD_WHITE) [$(RED)RM$(BOLD_WHITE)] $(NAME) Obejcts files$(CLEAR)"
+		@$(RM) $(OBJ)
+
+fclean:     	clean
+		@$(ECHO) "$(BOLD_WHITE) [$(RED)RM$(BOLD_WHITE)] $(NAME) Binary file$(CLEAR)\n"
+		@$(RM) $(NAME)
+
+re:		fclean all
+
+.cpp.o:
+		@$(CXX) $(CXXFLAGS) $(INC) $(LDFLAGS) -c $< -o $@
+		@$(ECHO) "$(BOLD_WHITE) [$(GREEN)OK$(BOLD_WHITE)] Compiled "$<"$(CLEAR)"
+
+.PHONY:		all clean fclean re
