f11ffd3a450b523c1f571677095d7b544c2417f6
PierreBougon <pierre.bougon@epitech.eu>
2017-04-25 18:51:44 +0200

    TCPSocket

---
+++ include/socket/TCPSocket.hpp
@@ -0,0 +1,21 @@
+//
+// Created by Pierre Bougon on 20/04/17.
+//
+
+#ifndef CPP_PLAZZA_TCPSOCKET_HPP_
+#define CPP_PLAZZA_TCPSOCKET_HPP_
+
+
+namespace plazza
+{
+    namespace network
+    {
+        class TCPSocket
+        {
+
+        };
+    }
+}
+
+
+#endif // !CPP_PLAZZA_TCPSOCKET_HPP_
---
+++ src/socket/TCPSocket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pierre Bougon on 20/04/17.
+//
+
+#include "socket/TCPSocket.hpp"
