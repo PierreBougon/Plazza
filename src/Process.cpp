6d494809be144ea4c0cf12ea65d814369826c8b8
Clement Peau <clement.peau@epitech.eu>
2017-04-26 16:16:37 +0200

    Updated TCPClient.hpp and Process.cpp and Makefile

--- src/Process.cpp
+++ src/Process.cpp
@@ -19,7 +19,7 @@ void plazza::Process::addNewTask(const plazza::network::Packet &packet) {
 	
 	if (tasks.size() == threadPool.getNumberOfThreads() * 2) {;//TODO RENVOYER UN PACKET D'ERREUR
 		plazza::network::Packet outputPacket(plazza::network::StatusCode::INTERNAL_SERVER_ERROR);
-		plazza::network::Client().send(outputPacket);
+		plazza::network::Client().getInstance().send(outputPacket, plazza::network::Client().getInstance().getSocket());
 	}
 	std::string first = packet.data.substr(0, packet.data.find(" "));
 	std::string second = packet.data.substr(packet.data.find(" ") + 1, packet.data.size());
