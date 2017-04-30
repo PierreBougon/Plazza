0a72d2ebc145330cc61243a8a03dd0441a1a0dc6
Clement Peau <clement.peau@epitech.eu>
2017-04-30 16:18:11 +0200

    !shouldQuit != shouldQuit

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ int main(int ac, char **av) {
 		}
 		if (isClient) {
 			plazza::Process ChildProcess(nbThreads);
-			while (ChildProcess.shouldQuit());
+			while (!ChildProcess.shouldQuit());
 		} else {
 			Logger::getInstance().setFile("logFile.txt");
 			plazza::ProcessHandler ProcessHandler(nbThreads, av[0]);
